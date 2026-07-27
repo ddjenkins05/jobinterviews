b7dddece5bb6d26679e0f3e6adee6d1f0f25b5d3
ddjenkins05 <ddjenkins05@yahoo.com>
2026-07-27 11:27:58 -0700

    Fixed nsg rules for Bastion

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -40,6 +40,19 @@ resource "azurerm_network_security_group" "web_test_nsg" {
   resource_group_name = var.resource_group_name
 
   security_rule {
+    name                       = "allow-bastion-rdp"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "*"
+
+  }
+
+  security_rule {
     name                       = "allow-https"
     priority                   = 101
     direction                  = "Inbound"
@@ -63,7 +76,7 @@ resource "azurerm_network_security_group" "web_test_nsg" {
   }
   security_rule {
     name                       = "block-rdp"
-    priority                   = 100
+    priority                   = 200
     direction                  = "Inbound"
     access                     = "Deny"
     protocol                   = "Tcp"
@@ -95,8 +108,21 @@ resource "azurerm_network_security_group" "db_test_nsg" {
   }
 
   security_rule {
-    name                       = "block-rdp"
+    name                       = "allow-bastion-rdp"
     priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "*"
+
+  }
+
+  security_rule {
+    name                       = "block-rdp"
+    priority                   = 200
     direction                  = "Inbound"
     access                     = "Deny"
     protocol                   = "Tcp"
@@ -118,4 +144,27 @@ resource "azurerm_subnet_network_security_group_association" "web_subnet_nsg" {
 resource "azurerm_subnet_network_security_group_association" "db_subnet_nsg" {
   subnet_id                 = azurerm_subnet.data_subnet_test.id
   network_security_group_id = azurerm_network_security_group.db_test_nsg.id
+}
+
+data "azurerm_virtual_network" "mgmt_network" {
+  name                = "vnet-management"
+  resource_group_name = "rg-management-infra"
+}
+
+output "id" {
+  value = azurerm_virtual_network_peering.tomagmt_vnet_peering.id
+}
+
+resource "azurerm_virtual_network_peering" "tomagmt_vnet_peering" {
+  name                      = "${azurerm_virtual_network.test.name}-to-mgmt"
+  resource_group_name       = var.resource_group_name
+  virtual_network_name      = azurerm_virtual_network.test.name
+  remote_virtual_network_id = data.azurerm_virtual_network.mgmt_network.id
+}
+
+resource "azurerm_virtual_network_peering" "frommgmt_vnet_peering" {
+  name                      = "management-to-${azurerm_virtual_network.test.name}"
+  resource_group_name       = data.azurerm_virtual_network.mgmt_network.resource_group_name
+  virtual_network_name      = data.azurerm_virtual_network.mgmt_network.name
+  remote_virtual_network_id = azurerm_virtual_network.test.id
 }
